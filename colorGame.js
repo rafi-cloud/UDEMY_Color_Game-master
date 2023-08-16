ef1c1728704ce13f2c3a404a5ee7b30a404ee7ab
rafi miazi <rafi.txt@gmail.com>
2023-08-16 17:03:10 +0600

    production ready

---
+++ colorGame.js
@@ -0,0 +1,93 @@
+let numSquares = 6;
+let colors = [];
+let pickedColor;
+let squares = document.querySelectorAll('.square');
+let colorDisplay = document.getElementById('colorDisplay');
+let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
+let resetButton = document.querySelector('#reset');
+let modeButtons = document.querySelectorAll('.mode');
+
+init();
+
+function setupModeButtons() {
+	for (i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener('click', function() {
+			modeButtons[0].classList.remove('selected');
+			modeButtons[1].classList.remove('selected');
+			this.classList.add('selected');
+			this.textContent === 'Easy' ? (numSquares = 3) : (numSquares = 6);
+			reset();
+		});
+	}
+}
+
+function setupSquares() {
+	for (i = 0; i < squares.length; i++) {
+		squares[i].addEventListener('click', function() {
+			let clickedColor = this.style.backgroundColor;
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = 'Correct!';
+				resetButton.textContent = 'Play Again!';
+				changeColors(pickedColor);
+				h1.style.backgroundColor = pickedColor;
+			} else {
+				this.style.backgroundColor = '#232323';
+				messageDisplay.textContent = 'Try again!';
+			}
+		});
+	}
+}
+
+function init() {
+	setupModeButtons();
+	setupSquares();
+	reset();
+}
+
+function reset() {
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = 'New Colors';
+	messageDisplay.textContent = '';
+	for (i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.display = 'block';
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = 'none';
+		}
+	}
+	h1.style.backgroundColor = 'steelblue';
+}
+
+resetButton.addEventListener('click', function() {
+	reset();
+});
+
+function changeColors(color) {
+	for (i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	let randIndex = Math.floor(Math.random() * colors.length);
+	return colors[randIndex];
+}
+
+function generateRandomColors(num) {
+	let arr = [];
+	for (i = 0; i < num; i++) {
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor() {
+	let r = Math.floor(Math.random() * 256);
+	let g = Math.floor(Math.random() * 256);
+	let b = Math.floor(Math.random() * 256);
+	return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
